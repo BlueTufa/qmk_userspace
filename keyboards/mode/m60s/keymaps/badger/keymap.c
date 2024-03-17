58da4c282edb933bb7dec7b596c283b32c059d3b
Dan White <github@bluetufa.com>
2024-03-17 09:48:00 -0600

    update copyright

--- keyboards/mode/m60s/keymaps/badger/keymap.c
+++ keyboards/mode/m60s/keymaps/badger/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Dan White <opensource@bluetufa.com>
+Copyright 2024 Dan White <opensource@bluetufa.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
